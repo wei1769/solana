8c70fe8ca2cc53a8295ef2e635b8450c6a08acce
Wei <wei@wei1769.com>
2023-08-27 17:06:20 +0800

    patch breaking change

--- sdk/program/src/pubkey.rs
+++ sdk/program/src/pubkey.rs
@@ -162,9 +162,7 @@ impl TryFrom<&str> for Pubkey {
 pub fn bytes_are_curve_point<T: AsRef<[u8]>>(_bytes: T) -> bool {
     #[cfg(not(target_os = "solana"))]
     {
-        curve25519_dalek::edwards::CompressedEdwardsY::from_slice(_bytes.as_ref())
-            .decompress()
-            .is_some()
+        curve25519_dalek::edwards::CompressedEdwardsY::from_slice(_bytes.as_ref()).is_ok()
     }
     #[cfg(target_os = "solana")]
     unimplemented!();
